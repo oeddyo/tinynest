8ff787d327b03e25545897369163ad98c85ad198
Eddie Xie <oeddyo@gmail.com>
2025-03-01 12:35:31 -0800

    Supabase

---
+++ app/utils/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const supabaseUrl = process.env.EXPO_PUBLIC_API_URL;
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl!, supabaseKey!, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+  }
+});

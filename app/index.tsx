50fdba314423d388e8b7075ecc46a40cc5e334bb
Eddie Xie <oeddyo@gmail.com>
2025-03-01 13:46:13 -0800

    WIP

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
-import { Text, View, Button } from "react-native";
+import { useState } from "react";
+import { Text, View, Button, TextInput } from "react-native";
 
 export default function Page() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View
       style={{
@@ -9,11 +13,20 @@ export default function Page() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button
-        title="Click me twice3"
-        onPress={() => alert('Button pressed!')}
+      <Text>Sign Up</Text>
+      <TextInput 
+        placeholder="Email" 
+        value={email} 
+        onChangeText={setEmail}
+        autoCapitalize="none"
       />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
+      <Button title="Sign Up" onPress={() => {}} />
     </View>
   );
 }

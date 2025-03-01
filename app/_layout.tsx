eed73b2c2f30610a5b5724095ac16685c8fe4562
Eddie Xie <oeddyo@gmail.com>
2025-03-01 13:42:41 -0800

    Lint for now

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,9 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="index" options={{ title: "hi" }} />
-
-  </Stack>
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "hi" }} />
+    </Stack>
+  );
 }

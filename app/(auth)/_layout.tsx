6f8e778f243222dcd5d19a8d3886d15a75d6caa1
Eddie Xie <oeddyo@gmail.com>
2025-03-01 14:15:36 -0800

    WIP

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const Layout = () => {
+  return <Tabs></Tabs>;
+};
+
+export default Layout;
+
+const styles = StyleSheet.create({});
--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,16 +1,15 @@
 
-
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Page = () => {
+const Profile = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>Profile</Text>
     </View>
   )
 }
 
-export default Page
+export default Profile
 
 const styles = StyleSheet.create({})

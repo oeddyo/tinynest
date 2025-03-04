c359c361fbacc6f69244fc95de57a3d2da8d21c6
Eddie Xie <oeddyo@gmail.com>
2025-03-03 21:41:00 -0800

    WIP

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // ...other plugins if any
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
+

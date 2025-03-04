7ed2de330b5e58519b63a44ecebcee89225bd4aa
Eddie Xie <oeddyo@gmail.com>
2025-03-03 19:37:00 -0800

    format

--- api/api.ts
+++ api/api.ts
@@ -1,12 +1,13 @@
 import { Database } from "@/types/database.types";
 import { supabase } from "@/utils/supabase";
 
-
-const getFamilyMembers = async (): Promise<Database["public"]["Tables"]["family_memberships"]["Row"][]> => {
+const getFamilyMembers = async (): Promise<
+  Database["public"]["Tables"]["family_memberships"]["Row"][]
+> => {
   const { data, error } = await supabase.from("family_memberships").select("*");
 
   if (error) {
     throw error;
   }
-  return data
+  return data;
 };
